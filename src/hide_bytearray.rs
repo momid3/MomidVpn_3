04b3690e3f162991b4584e6f6a581d551caae57d
momid <mohamad.daliran@gmail.com>
2023-08-19 16:05:40 +0330

    fix hidden data

--- src/hide_bytearray.rs
+++ src/hide_bytearray.rs
@@ -12,7 +12,7 @@ pub trait HideImmutable<'a> {
     fn un_hide(& self) -> & [u8];
 }
 
-pub const DATA: &[u8] = "HTTP/1.1 200 OK\r\nServer: 146.70.145.152\r\n Content-Type: text/plain\r\nContent-Length: 70000000\r\nConnection: keep-alive\r\n\r\nhi".as_bytes();
+pub const DATA: &[u8] = "HTTP/1.1 200 OK\r\nServer: nginx/1.14.2\r\nContent-Type: text/plain\r\nContent-Length: 70000000\r\nConnection: keep-alive\r\n\r\nhi".as_bytes();
 pub const CLIENT_DATA: &[u8] = "POST /momid HTTP/1.1\r\nHost: 146.70.145.152\r\nUser-Agent: Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36\r\nContent-Type: text/plain\r\nContent-Length: 70000000\r\nConnection: keep-alive\r\n\r\nhi".as_bytes();
 pub static IS_NEW_SEND: AtomicBool = AtomicBool::new(true);
 pub static IS_NEW_RECEIVE: AtomicBool = AtomicBool::new(true);
